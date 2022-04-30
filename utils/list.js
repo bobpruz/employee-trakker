d9cd3990a20dbd1b0d564bd47e866e3c2d8c8aa8
bobpruz <bobpruz@gmail.com>
2022-04-30 15:25:08 -0500

    updated SQL to join tables in list/view

--- utils/list.js
+++ utils/list.js
@@ -5,7 +5,7 @@ const db = require("../db/connection");
 const list = {
   // list departments
   listAllDepartments() {
-    const sql = `SELECT * FROM department`;
+    const sql = `SELECT id AS "ID", dept_name AS "dept_name" FROM department`;
 
     db.promise()
       .query(sql)
@@ -16,31 +16,31 @@ const list = {
       .catch((error) => console.log(error));
   },
 
-// list roles
-listAllRoles() {
-  const sql = `SELECT * FROM roles`;
+  // list roles
+  listAllRoles() {
+    const sql = `SELECT roles.id AS "ID", roles.title AS "Title", roles.salary AS "Salary", department.dept_name AS "Department" FROM roles left join department ON roles.department_id = department.id`;
 
-  db.promise()
-    .query(sql)
-    .then(([response]) => {
-      console.table(response);
-      app.promptUser();
-    })
-    .catch((error) => console.log(error));
-},
+    db.promise()
+      .query(sql)
+      .then(([response]) => {
+        console.table(response);
+        app.promptUser();
+      })
+      .catch((error) => console.log(error));
+  },
 
-// list employees
-listAllEmployees() {
-  const sql = `SELECT * FROM employee`;
+  // list employees
+  listAllEmployees() {
+    const sql = `SELECT employee.id AS "ID", employee.first_name AS "First Name", employee.last_name AS "Last Name", department.dept_name AS "Department", roles.title AS "Title", roles.salary AS "Salary", CONCAT_WS(' ', emp2.first_name, emp2.last_name ) AS "Manager" FROM employee left join roles ON employee.role_id = roles.id left join department ON roles.department_id = department.id left join employee AS emp2 ON employee.manager_id = emp2.id `;
 
-  db.promise()
-    .query(sql)
-    .then(([response]) => {
-      console.table(response);
-      app.promptUser();
-    })
-    .catch((error) => console.log(error));
-},
+    db.promise()
+      .query(sql)
+      .then(([response]) => {
+        console.table(response);
+        app.promptUser();
+      })
+      .catch((error) => console.log(error));
+  },
 };
 
 module.exports = list;
